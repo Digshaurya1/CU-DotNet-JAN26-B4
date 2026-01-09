9fb74e33e2ed4f3ca785bebbb7865faad5cc5c73
Digshaurya Singh <singhdigshaurya@gmail.com>
2026-01-09 15:12:41 +0530

    done with MVC

---
+++ DAY_4/TrainingPortal/TrainingPortal/Controllers/Course1Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrainingPortal.Controllers
+{
+    public class Course1Controller : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Courses()
+        {
+            return View();
+        }
+
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
+
+
