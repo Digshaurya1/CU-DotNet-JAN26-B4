a1020fae5570329105203e1783dce2d2ddd8ccd7
Digshaurya Singh <singhdigshaurya@gmail.com>
2026-01-12 14:01:12 +0530

    ad

---
+++ Day_6/ClassLibraryA/GreetingHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassLibraryA
+{
+    public class GreetingHelper
+    {
+        public static string GetGreet(string name){
+
+            if(name == null || name == " "){
+             return "Hello Guest";     
+        }
+            else{
+        return $"Hello {name}";
+
+        }
+            }
+    }
+}
---
+++ Day_6/GreetingApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibraryA;
+
+namespace GreetingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string S = Console.ReadLine();
+            Console.WriteLine(GreetingHelper.GetGreet(S));
+        }
+    }
+}
