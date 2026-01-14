a32267e500bc47b8325899b8c16eae7001a52fd1
Digshaurya Singh <singhdigshaurya@gmail.com>
2026-01-14 14:44:25 +0530

    day 8

---
+++ Day_8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day8project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter <UserName>|<LoginMessage>");
+            string[] s = Console.ReadLine().Split('|');
+            string name = s[0];
+            string msg = (s[1].Trim()).ToLower();
+
+            if (msg.Equals("login successful")) Console.WriteLine("LOGIN SUCCESS");
+
+            else if (msg.Contains("successful")) Console.WriteLine("LOGIN SUCCESS (CUSTOM MESSAGE)");
+
+            else Console.WriteLine("LOGIN FAILD");
+        }
+    }
+}
