47ae8402e8ece3d3b806301d918c8400230891dc
Digshaurya Singh <singhdigshaurya@gmail.com>
2026-01-16 15:37:47 +0530

    Day 8.2

---
+++ Day_8.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Channels;
+
+namespace Day8projectSecond
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<TransactionId>#<AccountHolderName>#<TransactionNarration>");
+            string[] arr = Console.ReadLine().Split("#");
+            string TransactionId = arr[0]; ;
+            string AccountHolderName = arr[1];
+            string TransactionNarration = (arr[2].Trim()).ToLower();
+            TransactionNarration = string.Join(" ",TransactionNarration.Split(" ",StringSplitOptions.RemoveEmptyEntries));
+
+            if (TransactionNarration == "cash deposit successful")
+            {
+                Console.WriteLine($"Transaction Id: {TransactionId}");
+                Console.WriteLine($"Account Holder: {AccountHolderName}");
+                Console.WriteLine($"Narration: {TransactionNarration}");
+                Console.WriteLine($"Category : STANDARD TRANSACTION");
+            }
+
+            else if (TransactionNarration.Contains("deposit") ||
+                TransactionNarration.Contains("withdrawal") ||
+                TransactionNarration.Contains("transfer"))
+            {
+                Console.WriteLine($"Transaction Id: {TransactionId}");
+                Console.WriteLine($"Account Holder: {AccountHolderName}");
+                Console.WriteLine($"Narration: {TransactionNarration}");
+                Console.WriteLine($"Category : CUSTOM TRANSACTION");
+            }
+            else
+            {
+                Console.WriteLine($"Transaction Id: {TransactionId}");
+                Console.WriteLine($"Account Holder: {AccountHolderName}");
+                Console.WriteLine($"Narration: {TransactionNarration}");
+                Console.WriteLine($"Category : NON-FINANCIAL TRANSACTION");
+            }
+
+        }
+    }
+}
