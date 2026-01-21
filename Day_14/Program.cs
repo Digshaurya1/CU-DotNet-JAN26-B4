b5ae01e21b8ce2bf60f510c5a586adcb48b17cee
Digshaurya Singh <singhdigshaurya@gmail.com>
2026-01-21 14:11:28 +0530

    DAY_14

---
+++ Day_14/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace oopsPractice
+{
+    public enum Dept { 
+      CSE,
+      AI,
+      IOT,
+    }
+
+    class Employee
+    {
+
+        int id;
+
+        private int sal;
+
+        public int Sal
+        {
+            get { return sal; }
+
+            set
+            {
+                if (value > 50000 && value < 90000)
+                    sal = value;
+            }
+        }
+
+        public string Dept { get; set; }
+
+        public void getId(int id)
+        {
+            this.id = id;
+        }
+
+        public void SetId()
+        {
+            Console.WriteLine(id);
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.Sal = 10;
+            Console.WriteLine(emp.Sal);
+            emp.getId(11);
+            emp.SetId();
+            emp.Dept = "OS";
+            Console.WriteLine(emp.Dept);
+
+        }
+    }
+}
