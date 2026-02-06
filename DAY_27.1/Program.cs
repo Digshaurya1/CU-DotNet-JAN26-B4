a9c3d59cd0b74f0f05e6bdf14386a8645f0db9dd
Digshaurya Singh <singhdigshaurya@gmail.com>
2026-02-06 20:12:04 +0530

    DONE

---
+++ DAY_27.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Specialized;
+
+namespace Day_27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"file0.txt";
+            string direct = @"..\..\..\";
+            using StreamWriter  sw = new StreamWriter(direct + file,true);
+            Console.WriteLine("Enter the name");
+            string name = Console.ReadLine();
+            
+                sw.WriteLine(name);
+            
+        }
+    }
+}
